0ad2271f28c7eed9e03e26b710fc97b3889c026a
UniverseDevel <universedevel.help@outlook.com>
2022-02-17 11:37:26 +0100

    Cleanup

--- RPA Parser/RPA_Parser.cs
+++ RPA Parser/RPA_Parser.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using Ionic.Zlib;
